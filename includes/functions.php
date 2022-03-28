1b866175a15200c019ae50c81b8e73976cbe6249
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-03-28 20:57:33 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -325,20 +325,21 @@ function getIp()
 {
     $ip = '1.1.1.1';
     if (!empty($_SERVER['HTTP_X_REAL_IP'])) {
-        $ip = $_SERVER['HTTP_X_REAL_IP'];
-    } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else if (!empty($_SERVER['REMOTE_ADDR'])) {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    } else if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        $ip = $_SERVER['HTTP_X_REAL_IP']; 
+    } 
+    else if (!empty($_SERVER['HTTP_CLIENT_IP'])) { 
+        $ip = $_SERVER['HTTP_CLIENT_IP']; 
+    } 
+    else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; 
+    } 
+    else {
         $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-    } else {
-        $ip = 'unknown';
     }
-
-    //if ($ip == '::1') {
-    //    $ip = '1.1.1.1';
-    //}
+    
+    if ($ip == "::1" || $ip == "127.0.0.1") {
+        $ip = '1.1.1.1';
+    }
 
     return $ip;
 }
